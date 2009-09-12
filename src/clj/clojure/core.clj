a235ae29505d3d767f6609c4b7be2e1048a806ce
Jarkko Oranen <chousuke@gmail.com>
2009-09-12 15:38:10 +0300

    Add syntax-quote macro to core.clj

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -540,6 +540,119 @@
 
 ;;;;;;;;;;;;;;;;at this point all the support for syntax-quote exists;;;;;;;;;;;;;;;;;;;;;;
 
+(def #^{:private true} *splice-allowed* false)
+(def #^{:private true} *sq-gensyms* nil)
+
+(defn is-call? {:private true} [operator form]
+  (if (seq? form) 
+    (.equals (first form) operator)))
+
+;; simple versions needed here; will be redefined later
+
+(defn dec [x]
+  (clojure.lang.Numbers/minus x 1))
+
+(defn real-namespace {:private true} [ns-name]
+  (if ns-name
+    (str ((.getAliases *ns*) (symbol ns-name) ns-name))
+    ns-name))
+
+(defn remove-dot {:private true} [the-name]
+  (if (.endsWith the-name ".")
+    (.substring the-name 0 (dec (.length the-name)))
+    the-name))
+
+(defn var->sym {:private true} [v]
+  (symbol (str (.ns v)) (str (.sym v))))
+
+(defn qualify-symbol
+  "Works all kinds of magic to resolve a symbol to its fully qualified form"
+  {:private true}
+  [sym]
+  (let [the-ns (real-namespace (namespace sym))
+        the-name (remove-dot (name sym))
+        ns-res (when the-ns (.getMapping *ns* (symbol the-ns)))
+        res (.getMapping *ns* (symbol the-name))]
+
+    (cond
+      ;; there is no need to care about the namespace in this case;
+      ;; the original syntax-quote did not resolve names containing dots
+      ;; No "and" available yet...
+      (when (not res)
+        (.contains (name sym) "."))
+      , sym
+      (var? res)         (var->sym res)
+      (class? ns-res)    (symbol (.getName ns-res) the-name)
+      (when (not the-ns)
+        (class? res))    (symbol (str (.getName res)
+                                      (when (.endsWith the-name ".") ".")))
+      the-ns             (symbol the-ns the-name)
+      :else              (symbol (str *ns*) the-name))))
+
+(defn sq-w
+  "syntax-quote helper; everything is wrapped in a list
+  so that splicing things is easier"
+  {:private true}
+  ([form]
+     (list 'clojure.core/list form))
+  ([quote? form]
+     (list 'clojure.core/list (list 'quote form))))
+
+(def syntax-quote*)
+
+(defn sq-seq 
+  {:private true}
+  ([form] (sq-seq form false))
+  ([form splicable?]
+     (try
+      (clojure.lang.Var/pushThreadBindings {#'*splice-allowed* splicable?})
+      ; no doall here so needs a loop to enforce strictness
+      (list* 'clojure.core/concat (loop [f form a []]
+                                    (if (seq f) 
+                                      (recur (next f) (conj a (syntax-quote* (first f))))
+                                      a))) 
+      (finally
+       (clojure.lang.Var/popThreadBindings)))))
+
+(defn sq-sym
+  {:private true} 
+  [sym]
+  (if (not (.endsWith (name sym) "#"))
+    (qualify-symbol sym)
+    (let [auto (*sq-gensyms* (name sym))]
+      (if auto
+        auto
+        (let [new-sym 
+              (gensym (apply str (concat (butlast (name sym)) "__auto__")))]
+          (set! *sq-gensyms* (assoc *sq-gensyms* (name sym) new-sym))
+          new-sym)))))  
+  
+(defn syntax-quote*
+  {:private true} 
+  [form]
+  (cond
+    (special-symbol? form) (sq-w :q form)
+    (is-call? 'clojure.core/unquote form) 
+    , (sq-w (second form))
+    (is-call? 'clojure.core/unquote-splicing form) 
+    , (if *splice-allowed* 
+        (list* 'do (rest form)) 
+        (throw (IllegalStateException. "Splice not in sequential collection"))) 
+     
+    (is-call? 'syntax-quote form) (sq-w :q form)
+    (symbol? form) (sq-w :q (sq-sym form))
+    (vector? form) (sq-w (list 'clojure.core/vec (sq-seq form :splicable)))
+    (map? form) (sq-w (list 'clojure.core/apply 'clojure.core/hash-map 
+                         (sq-seq (apply concat (seq form)))))
+    (set? form) (sq-w (list 'clojure.core/apply 'clojure.core/hash-set (sq-seq form :splicable)))
+    (seq? form) (sq-w (sq-seq form :splicable))
+    :else (sq-w (list 'quote form))))
+
+(defmacro syntax-quote [form]
+  (try
+   (clojure.lang.Var/pushThreadBindings {#'*sq-gensyms* {}})
+   (list 'clojure.core/first (syntax-quote* form))
+   (finally (clojure.lang.Var/popThreadBindings)))) 
 
 (defmacro delay
   "Takes a body of expressions and yields a Delay object that will
